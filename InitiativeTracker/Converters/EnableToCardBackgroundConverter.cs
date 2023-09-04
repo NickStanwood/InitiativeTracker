77ee2864a1f0fde4f4f38e1f984df6f45d0e235d
Nick Stanwood <nstanwood1@gmail.com>
2023-09-03 22:29:11 -0600

    moved start button

---
+++ InitiativeTracker/Converters/EnableToCardBackgroundConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace InitiativeTracker
+{
+    public class EnableToCardBackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                bool enabled = (bool)value;
+                if (enabled)
+                    return (SolidColorBrush)Application.Current.TryFindResource("ActiveBackground");
+                return (SolidColorBrush)Application.Current.TryFindResource("InactiveBackground");
+            }
+            throw new InvalidOperationException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
