e12b403a12f96bd722bf732a4a2d976f056d8e19
Nick Stanwood <nstanwood1@gmail.com>
2023-09-24 18:33:53 -0600

    added characterPropertyModel

---
+++ InitiativeTracker/Models/CharacterPropertyModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace InitiativeTracker
+{
+    public class CharacterPropertyModel
+    {
+        [XmlAttribute]
+        public string Name { get; set; }
+
+        [XmlAttribute]
+        public string Value { get; set; }
+    }
+}
